7206d580a8b9275dc9ed7ec6f69a25b0a75ece7e
zzqs-mac-mini <597238072@qq.com>
2023-10-22 11:52:58 +0800

    提交保存

---
+++ flutter_vlc_player_platform_interface/lib/src/enums/media_event_type.dart
@@ -0,0 +1,14 @@
+// ignore: prefer-match-file-name
+enum VlcMediaEventType {
+  opening,
+  playing,
+  paused,
+  stopped,
+  buffering,
+  recording,
+  timeChanged,
+  mediaChanged,
+  ended,
+  unknown,
+  error,
+}
